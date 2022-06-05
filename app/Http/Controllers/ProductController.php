66e71b92c83bb12055047c06dfb80f16283ee198
rider <myintthwaykhine127@gmail.com>
2022-06-05 15:14:26 +0630

    build REST Api with validation

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::latest()->get();
+    }
+
+    public function show($id)
+    {
+        $exitingProduct = Product::find($id);
+        if($exitingProduct){
+            return $exitingProduct;
+        }
+        return "product not found";
+    }
+
+    public function store()
+    {
+        $validator = Validator::make(request()->all(),[
+            'name' => ['required','string',Rule::unique('products','name')],
+            'price' => 'required|numeric',
+        ]);
+
+        if($validator->fails()){
+            return $validator->errors();
+        }
+
+        $product =  Product::create([
+            'name' => request('name'),
+            'price' => request('price'),
+        ]);
+
+        return $product;
+    }
+
+    public function update($id)
+    {
+        $exitingProduct = Product::find($id);
+        if($exitingProduct){
+
+            $validator = Validator::make(request()->all(),[
+                'name' => ['nullable','string',Rule::unique('products','name')->ignore($id)],
+                'price' => ['nullable','numeric'],
+            ]);
+
+            if($validator->fails()){
+                return $validator->errors();
+            }
+
+            $exitingProduct->update(request()->only('name','price'));
+
+            return $exitingProduct;
+        }
+
+        return 'product not found';
+    }
+
+    public function destroy($id)
+    {
+        $exitingProduct = Product::find($id);
+        if($exitingProduct){
+            $exitingProduct->delete();
+            return "delete successfully";
+        }
+
+        return 'product not found';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(ProductController::class)->group(function(){
+    Route::get('/products','index');
+    Route::prefix('/product')->group(function(){
+        Route::get('/{id}','show');
+        Route::post('/store','store');
+        Route::put('/{id}','update');
+        Route::delete('/{id}','destroy');
+    });
 });
